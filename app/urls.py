ba20b80fe7dd50cb20026b8bfcabfaaf842ed17a
ayoubbif <ayoub.biferdou@e-polytechnique.ma>
2024-10-31 05:04:35 +0100

    Update URL routing: add UtilityRateView and change index to HomeView

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import UtilityRateView, HomeView
 
 urlpatterns = [
-    path('', index),
+    path('', HomeView.as_view(), name='home'),
+    path('api/utility-rates/', UtilityRateView.as_view(), name='utility-rates'),
 ]
