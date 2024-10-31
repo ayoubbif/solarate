3e529353880c959ec5b67e6400002173a5fdeaae
ayoubbif <ayoub.biferdou@e-polytechnique.ma>
2024-10-31 02:10:50 +0100

    Add serializers for Project and ProposalUtility models

---
+++ app/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Project, ProposalUtility
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = [
+            'id', 'user', 'name', 'description', 'address', 'consumption',
+            'percentage', 'created_at', 'updated_at'
+        ]
+        read_only_fields = ['created_at', 'updated_at']
+
+class ProposalUtilitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProposalUtility
+        fields = [
+            'id', 'project', 'openei_id', 'rate_name',
+            'approved', 'is_default', 'start_date',
+            'pricing_matrix', 'average_rate', 'first_year_cost'
+        ]
