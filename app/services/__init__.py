0f11f91ce89c9e26dd0c333e3f9fabcfaddd1717
ayoubbif <ayoub.biferdou@e-polytechnique.ma>
2024-11-01 05:17:31 +0100

    Replace CostCalculator with RateCalculator in service exports

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,12 +1,12 @@
 from .rate_provider import RateDataProvider, OpenEIRateProvider
 from .rate_processor import RateProcessor
-from .cost_calculator import CostCalculator
+from .rate_calculator import RateCalculator
 from .input_validator import InputValidator
 
 __all__ = [
     'RateDataProvider',
     'OpenEIRateProvider',
     'RateProcessor',
-    'CostCalculator',
+    'RateCalculator',
     'InputValidator'
 ]
