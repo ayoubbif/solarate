e357d12853d1e9a67a97ac7b1d50dfdfe1136553
ayoubbif <ayoub.biferdou@e-polytechnique.ma>
2024-11-01 05:17:45 +0100

    Add ProjectAPIView to views module for project event handling

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,4 +1,5 @@
 from .home_view import HomeView
 from .utility_rate_view import UtilityRateView
+from .project_webhook_view import ProjectAPIView
 
-__all__ = ['HomeView', 'UtilityRateView']
+__all__ = ['HomeView', 'UtilityRateView', 'ProjectAPIView']
